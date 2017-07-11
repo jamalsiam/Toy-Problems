c948eedca0a548ec9350cf9b6680e59e92916cbf
jamalsiam <jamalsiam1995@gmail.com>
2017-07-11 16:03:51 +0300

    solved

--- Bubble Sort/bubblesort.js
+++ Bubble Sort/bubblesort.js
@@ -1,5 +1,6 @@
 arr=[4,5,234,656,2,7,9];
 let swap = true;
+var count = 0;
 while(swap){
 	swap = false;
 	for(let i=0; i < arr.length; i++){
@@ -8,7 +9,8 @@ while(swap){
 			arr[i+1]=arr[i];
 			arr[i]=temp;
 			swap = true;
+			count++;
 		}
 	}
 }
-console.log(arr)
+console.log(arr,count)
---
+++ selection sort/selection sort.js
@@ -0,0 +1,27 @@
+arr=[4,5,234,656,2,7,9];
+
+
+for (var i = 0; i < arr.length; i++) {
+  for (var x = i; x < arr.length; x++) {
+        if(arr[i]>arr[x]){
+          var t=arr[i];
+          arr[i]=arr[x];
+          arr[x]=t;
+        }
+
+      
+
+      }   
+}
+
+
+ console.log(arr);
+
+
+
+
+
+
+
+
+

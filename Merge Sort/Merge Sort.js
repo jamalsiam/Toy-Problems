8e324ce82800d56b056896f9074ea65b5da44166
jamalsiam <jamalsiam1995@gmail.com>
2017-07-12 14:14:34 +0300

    solved

---
+++ Merge Sort/Merge Sort.js
@@ -0,0 +1,42 @@
+arr=[23,2,1,4,44,12,7];
+
+
+
+function mergeSort(arr){
+
+	if (arr.length<=1) 
+	
+		return arr;
+	
+	var nos=Math.floor(arr.length/2);
+	var firstList=arr.slice(0,nos);
+	var secondList=arr.slice(nos,arr.length);;
+
+	return merge(mergeSort(secondList),mergeSort(firstList));
+}
+
+function merge(firstArr,lastArr){
+	var sortedArr=[];
+	while(firstArr.length && lastArr.length){
+		if(firstArr[0]<=lastArr[0])
+			{sortedArr.push(firstArr.shift());}
+		else
+			{sortedArr.push(lastArr.shift());}
+	}
+	while(firstArr.length){
+		sortedArr.push(firstArr.shift());
+
+	}
+	while(lastArr.length){
+		sortedArr.push(lastArr.shift());
+
+	}
+	return sortedArr;
+}
+
+
+
+
+
+
+console.log(mergeSort(arr))

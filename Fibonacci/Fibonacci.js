2b65f9ce9ef095456f81f174563d0cc0b170590a
jamalsiam <jamalsiam1995@gmail.com>
2017-07-25 11:38:31 +0300

    Solved

---
+++ Fibonacci/Fibonacci.js
@@ -0,0 +1,21 @@
+/**
+Fibonacci
+ */
+
+/**
+  * example usage:
+  * fib(i)=fib(i-1)+fib(i+2)
+  * 
+  */
+
+  var fib = function(index) {
+  	if(index<=1)
+  		return index;
+
+  	return a=fib(index-1)+fib(index-2)
+  
+  }
+
+
+
+ console.log(fib(6))
